45cb886ea750ef043002f53b2aa161370594fee9
Ashaba Derrick <derrick@victorium.biz>
2021-07-10 16:52:29 +0300

    setup splash screen setup loginscreen and sign up screen, no firebase functionality

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,17 @@
 import 'package:buthings/screens/home.dart';
+import 'package:buthings/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Bu Things',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: HomeScreen(),
+      home: SafeArea(child: LoginScreen()),
     );
   }
 }
